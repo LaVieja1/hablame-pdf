4a54dfff83d4ecc3a3ac233a21e12bee2d688635
santicano <canosantiago404@gmail.com>
2024-02-09 12:57:06 -0300

    instalado svelte y flowbite

---
+++ src/components/App.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Alert } from 'flowbite-svelte';
+</script>
+
+<Alert color="dark">
+  <span class="font-medium">Dark alert!</span>
+  Change a few things up and try submitting again.
+</Alert>
