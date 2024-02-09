4f1bd3c52a3b89c632fed244cc66d576704db5fa
santicano <canosantiago404@gmail.com>
2024-02-09 20:00:33 -0300

    esqueleto de stepChat

--- src/components/StepChat.svelte
+++ src/components/StepChat.svelte
@@ -1,8 +1,10 @@
 <script>
   import { Input, Label, Spinner } from "flowbite-svelte";
   import { appStatusInfo } from "../store.ts";
+  import { setAppStatusError } from "../store.ts";
   const { url, pages, id } = $appStatusInfo;
 
+  let answer = "";
   let loading = false;
 
   const numOfImagesToShow = Math.min(pages, 4);
@@ -13,7 +15,39 @@
       .replace(".pdf", ".jpg");
   });
 
-  const handleSubmit = () => {};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    loading = true;
+
+    const question = event.target.question.value;
+
+    try {
+      const res = await fetch("/api/ask", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          id,
+          question: "De qué trata este PDF?",
+        }),
+      });
+
+      if (!res.ok) {
+        loading = false;
+        console.error("Error en la llamada a la API");
+        return;
+      }
+
+      const { answer: apiAnswer } = await res.json();
+      answer = apiAnswer;
+    } catch (e) {
+      setAppStatusError();
+    } finally {
+      loading = false;
+    }
+  };
 </script>
 
 <div class="grid grid-cols-4 gap-2">
@@ -27,9 +61,22 @@
 </div>
 
 <form class="mt-8" on:submit={handleSubmit}>
-  <Label for="question-input" class="block mb-2 text-slate-200"
+  <Label for="question" class="block mb-2 text-slate-200"
     >Pregúntale al bot</Label
   >
-  <Input id="question-input" required placeholder="¿De qué trata este PDF?"
-  ></Input>
+  <Input id="question" required placeholder="¿De qué trata este PDF?"></Input>
 </form>
+
+{#if loading}
+  <div class="flex justify-center items-center flex-col gap-y-2">
+    <Spinner color="purple" />
+    <p class="opacity-75">Esperando respuesta...</p>
+  </div>
+{/if}
+
+{#if answer}
+  <div class="mt-8">
+    <p class="font-medium">Respuesta:</p>
+    <p>{answer}</p>
+  </div>
+{/if}
---
+++ src/pages/api/ask.ts
@@ -0,0 +1,9 @@
+import type { APIRoute } from "astro";
+
+export const POST: APIRoute = async ({ request }) => {
+  return new Response(
+    JSON.stringify({
+      response: "",
+    })
+  );
+};
