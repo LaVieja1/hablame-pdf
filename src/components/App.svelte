9257232773db6c4e9fa71059344fad199d8b1f10
santicano <canosantiago404@gmail.com>
2024-02-09 13:36:36 -0300

    añadidos steps

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,20 +1,23 @@
 <script>
   import { Alert } from "flowbite-svelte";
   import { APP_STATUS, appStatus } from "../store.ts";
+  import StepUpload from "./StepUpload.svelte";
+  import StepLoading from "./StepLoading.svelte";
+  import StepChat from "./StepChat.svelte";
 </script>
 
 <!-- Bloque IF-->
 {#if $appStatus === APP_STATUS.INIT}
-  <p>La página inicial</p>
+  <StepUpload />
 {:else if $appStatus === APP_STATUS.LOADING}
-  <p>Cargando...</p>
+  <StepLoading />
 {:else if $appStatus === APP_STATUS.ERROR}
   <Alert color="dark">
     <span class="font-medium">Ups...</span>
     Hay un error en la aplicación
   </Alert>
 {:else if $appStatus === APP_STATUS.CHAT_MODE}
-  <p>Chat mode</p>
+  <StepChat />
 {:else}
   <Alert color="dark">
     <span class="font-medium">Acción desconocida</span>
