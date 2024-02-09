8632d50ca45a8a27973cec218afb1e5ed709ec7d
santicano <canosantiago404@gmail.com>
2024-02-09 13:34:27 -0300

    iniciada store

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,8 +1,23 @@
 <script>
-  import { Alert } from 'flowbite-svelte';
+  import { Alert } from "flowbite-svelte";
+  import { APP_STATUS, appStatus } from "../store.ts";
 </script>
 
-<Alert color="dark">
-  <span class="font-medium">Dark alert!</span>
-  Change a few things up and try submitting again.
-</Alert>
+<!-- Bloque IF-->
+{#if $appStatus === APP_STATUS.INIT}
+  <p>La página inicial</p>
+{:else if $appStatus === APP_STATUS.LOADING}
+  <p>Cargando...</p>
+{:else if $appStatus === APP_STATUS.ERROR}
+  <Alert color="dark">
+    <span class="font-medium">Ups...</span>
+    Hay un error en la aplicación
+  </Alert>
+{:else if $appStatus === APP_STATUS.CHAT_MODE}
+  <p>Chat mode</p>
+{:else}
+  <Alert color="dark">
+    <span class="font-medium">Acción desconocida</span>
+    Este estado de la aplicación no esta reconocido
+  </Alert>
+{/if}
