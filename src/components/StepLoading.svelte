c4472bbfed2c11d373c4648a557e6783a24f18d9
santicano <canosantiago404@gmail.com>
2024-02-09 14:54:59 -0300

    añadido estado de cargando cuando se sube un pdf

---
+++ src/components/StepLoading.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Spinner } from "flowbite-svelte";
+</script>
+
+<div class="flex justify-center items-center flex-col gap-y-2">
+  <Spinner color="purple" />
+  <p class="opacity-75">Subiendo el archivo...</p>
+</div>
--- src/components/StepUpload.svelte
+++ src/components/StepUpload.svelte
@@ -1,1 +1,29 @@
-Ayiyi
+<script>
+  import { appStatus, setAppStatusLoading } from "../store.ts";
+
+  import Dropzone from "svelte-file-dropzone";
+
+  let files = {
+    accepted: [],
+    rejected: [],
+  };
+
+  function handleFilesSelect(e) {
+    const { acceptedFiles, fileRejections } = e.detail;
+    files.accepted = [...files.accepted, ...acceptedFiles];
+    files.rejected = [...files.rejected, ...fileRejections];
+
+    if (acceptedFiles.length > 0) {
+      setAppStatusLoading();
+    }
+  }
+</script>
+
+<Dropzone multiple={false} accept="application/pdf" on:drop={handleFilesSelect}
+  >Arrastra y suelta aquí tu archivo PDF</Dropzone
+>
+<ol>
+  {#each files.accepted as item}
+    <li>{item.name}</li>
+  {/each}
+</ol>
